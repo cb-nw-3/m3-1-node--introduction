e4a9493e77070499bbbdd166d798334de5985dab
Mae <maeberris@outlook.com>
2020-05-30 15:25:52 -0400

    completed to exercise 6

--- __workshop/exercisesP1.js
+++ __workshop/exercisesP1.js
@@ -15,7 +15,7 @@ const q2 = (req, res) => {
   // pass this variable to the ejs file and render it in the answer box.
   const sentence = '<p>This is the way.</p>';
 
-  res.render('pages/question2', {});
+  res.render('pages/question2', {sentence});
 };
 // -----------------------------------------------
 
@@ -33,7 +33,7 @@ const q3 = (req, res) => {
     favoriteBeverage: 'Duff Beer',
   };
 
-  res.render('pages/question3');
+  res.render('pages/question3', {homer: homer});
 };
 // -----------------------------------------------
 
@@ -42,7 +42,11 @@ const q3 = (req, res) => {
 const q4 = (req, res) => {
   const popularGirlNames = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica'];
 
-  res.render('pages/question4');
+  res.render('pages/question4',{
+    name1: popularGirlNames[0],
+    name2: popularGirlNames[1],
+    name3: popularGirlNames[3],
+  });
 };
 // -----------------------------------------------
 
@@ -51,7 +55,7 @@ const q4 = (req, res) => {
 const q5 = (req, res) => {
   const popularGirlNames = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica'];
 
-  res.render('pages/question5');
+  res.render('pages/question5', {popularGirlNames});
 };
 // -----------------------------------------------
 
