caaea14b11904b8c03210c1e1aab708576c835b6
rogerlam <rogerlam@wylco.org>
2020-05-30 11:17:52 -0400

    initial commit up to q6 complete

--- __workshop/exercisesP1.js
+++ __workshop/exercisesP1.js
@@ -15,7 +15,7 @@ const q2 = (req, res) => {
   // pass this variable to the ejs file and render it in the answer box.
   const sentence = '<p>This is the way.</p>';
 
-  res.render('pages/question2', {});
+  res.render('pages/question2', { sentence: sentence });
 };
 // -----------------------------------------------
 
@@ -33,7 +33,7 @@ const q3 = (req, res) => {
     favoriteBeverage: 'Duff Beer',
   };
 
-  res.render('pages/question3');
+  res.render('pages/question3', { homer: homer });
 };
 // -----------------------------------------------
 
@@ -42,7 +42,13 @@ const q3 = (req, res) => {
 const q4 = (req, res) => {
   const popularGirlNames = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica'];
 
-  res.render('pages/question4');
+  let topThreeNames = [];
+
+  for (let i = 0; i < 3; i++) {
+    topThreeNames.push(popularGirlNames[i]);
+  }
+
+  res.render('pages/question4', { topThreeNames: topThreeNames });
 };
 // -----------------------------------------------
 
@@ -51,7 +57,7 @@ const q4 = (req, res) => {
 const q5 = (req, res) => {
   const popularGirlNames = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica'];
 
-  res.render('pages/question5');
+  res.render('pages/question5', { popularGirlNames: popularGirlNames });
 };
 // -----------------------------------------------
 
